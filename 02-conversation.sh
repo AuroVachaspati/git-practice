f748b1727a83fa27b5e807d0212ddace0d555047
Vachas <vachaspathy.s@gmail.com>
2025-12-11 15:43:01 +0530

    Joyful conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Ramesh:: Hi, Suresh! How are you?"
+echo "Suresh:: Hi, Ramesh! I'm good, thanks! How about you?"
+echo "Ramesh:: I'm doing well too. What have you been up to lately?"
+echo "Suresh:: Just working on some projects and spending time with family. You?"
