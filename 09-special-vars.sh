5fc32ede785f05b9a5f924dc28682b0bc416842f
Vachas <vachaspathy.s@gmail.com>
2025-12-11 20:02:55 +0530

    variables

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -9,3 +9,5 @@ echo "name of the your script: $0"
 echo "script's process id is: $$"
 echo "current working directory is: $PWD"
 echo "Home diretory of the current use is : $HOME"
+sleep 100 & 
+echo "process id of the last background command is: $!"
