f43c20612c25f972f63f3cc4917a8442dfb5bc65
Vachas <vachaspathy.s@gmail.com>
2025-12-11 23:10:09 +0530

    variables

--- 11-installation.sh
+++ 11-installation.sh
@@ -2,4 +2,13 @@
 
 
 USERID=$(id -u)
-echo "Your user ID is: $USERID"
+#echo "Your user ID is: $USERID"
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run the script with root privileges"
+fi
+dnf install git -y
+echo "starting installation..."
+
+
