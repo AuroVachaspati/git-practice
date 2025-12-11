ee76ee6b534fd0bfbb35a465347201cde9bbc726
Vachas <vachaspathy.s@gmail.com>
2025-12-11 16:34:48 +0530

    variables

---
+++ 06-array.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+FRUITS=("apple" "banana")
+
+#index starts from 0, size is 3
+
+echo first fruit is:  ${FRUITS[0]}
+echo second fruit is: ${FRUITS[1]}
+
+echo all fruits: ${FRUITS[@]}
