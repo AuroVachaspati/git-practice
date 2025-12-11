58cb8bfb0dc8391f5d9cd7d4a893ea6efec58025
Vachas <vachaspathy.s@gmail.com>
2025-12-11 19:57:11 +0530

    variables

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -2,3 +2,10 @@
  
 
 echo "all the variables passed to the script: $@"
+
+echo "number of variables passed to the script: $#"
+
+echo "name of the your script: $0"
+echo "script's process id is: $$"
+
+
