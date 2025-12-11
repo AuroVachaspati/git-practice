cd825ba42b3b441d45ec44c3137b769bffad1bef
Vachas <vachaspathy.s@gmail.com>
2025-12-12 00:02:01 +0530

    variables

--- 11-installation.sh
+++ 11-installation.sh
@@ -7,10 +7,17 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "Please run the script with root privileges."
-    #exit 1
+    exit 1
 fi
 
-echo "starting installation..."
-dnf install git -y
+dnf list installed git
+if [ $? -ne 0 ]
+then
+    echo "Git is not installed, going to install it now.."
+    dnf install git -y
+    echo "Git installation completed."
+else
+    echo "Git is already installed."
+fi
 
 
