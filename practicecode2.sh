8f50f1adf24a195da8bdee8cd93dded9e1dbee66
Vachas <vachaspathy.s@gmail.com>
2025-12-12 15:55:10 +0530

    variables

--- practicecode2.sh
+++ practicecode2.sh
@@ -4,33 +4,42 @@ echo "Has the student written the exam (yes/no)? :"
 read exam_written
 
 #LEVEL1 - Check if the student wrote the exam
-if [ $exam_written = "yes" ]
+if [ "$exam_written" = "yes" ]
 then
     echo "Student has written the exam. Thanks"
     echo "Please enter the marks secured :"
     read marks
 
-#LEVEL2 - Check whether the student has passed the exam
-    if [ $marks -gt 33 ]
+    #LEVEL2 - Check whether the student has passed the exam
+    if [ "$marks" -gt 33 ]
     then 
         echo "You have passed the exam!"
-        #LEVEL3 - Check whether the student has passed the exam with distinction
-        if [ $marks -gt 70 ]
+
+        #LEVEL3 - Check for First Class (>90)
+        if [ "$marks" -gt 90 ]
         then 
-            echo "You have passed the exam in distinction"
-        else
-            echo "Distinction is between 70-90"
-            if [ $marks -gt 90 ]
-            then 
-                echo "Congratulations! You have passed the exam in first class"
+            echo "Congratulations! FIRST CLASS with $marks marks! 🏆"
+            
+            #LEVEL4 - Check for Outstanding (>95)
+            if [ "$marks" -gt 95 ]
+            then
+                echo "OUTSTANDING! You're a top scorer!"
             else
-                echo "Great, Try to get first class, you have already passed in distinction"
+                echo "Excellent work! Keep it up!"
             fi
             #LEVEL4 END
+            
+        #LEVEL3 - Check for Distinction (>70)
+        elif [ "$marks" -gt 70 ]
+        then
+            echo "You have passed the exam with DISTINCTION!"
+        else
+            echo "You passed! Good job!"
         fi
-        #LEVEL2 END
+        #LEVEL3 END
+        
     else
-        echo "You have not passed the exam, please check"
+        echo "You have not passed the exam (need >33), please check"
     fi
     #LEVEL2 END
 else
