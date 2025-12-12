e54951adb97dc3a3104750451d7ac7b03dcc43e9
Vachas <vachaspathy.s@gmail.com>
2025-12-12 15:14:27 +0530

    variables

--- 11-installation.sh
+++ 11-installation.sh
@@ -1,47 +1,38 @@
 #!/bin/bash
 
-# USERID=$(id -u)
-# #echo "User ID is: $USERID"
+USERID=$(id -u)
+#echo "User ID is: $USERID"
 
 
-# if [ $USERID -ne 0 ]
-# then
-#     echo "Please run this script with root priveleges"
-#     exit 1
-# fi
-
-
-
-# dnf list installed git # Just checking whether installed or not
-
-# if [ $? -ne 0 ]
-# then
-#     echo "Git is not installed, going to install it.."
-#     dnf install git -y # here installing
-
-#     if [ $? -ne 0 ]
-#     then
-#         echo "Git installation is not success...check it"
-#         exit 1
-#     else
-#         echo "Git installation is success"
-#     fi
-
-# else
-#     echo "Git is already installed, will check for latest version"
-# fi
-
-
-
-echo "Checking Git status..."
-echo
-
-# Check if git is installed
-if ! command -v git &> /dev/null; then
-    echo "Git is not installed. Installing..."
-    sudo dnf install -y git
-    exit 0
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root priveleges"
+    exit 1
 fi
+
+
+
+dnf list installed git # Just checking whether installed or not
+
+if [ $? -ne 0 ]
+then
+    echo "Git is not installed, going to install it.."
+    dnf install git -y # here installing
+
+    if [ $? -ne 0 ]
+    then
+        echo "Git installation is not success...check it"
+        exit 1
+    else
+        echo "Git installation is success"
+    fi
+
+else
+    echo "Git is already installed, will check for latest version"
+fi
+
+
+
 
 # Show current version
 current_version=$(git --version)
@@ -56,3 +47,21 @@ else
     echo
     echo "Updated: $(git --version)"
 fi
+
+
+#Check whether mysql is installed or not
+
+dnf list installed mysql-server
+if [ $? -ne 0 ]
+then
+    echo "Mysql is not installed, going to install it."
+    dnf install mysql server -y
+    if [ $? -ne 0 ]
+    then
+        echo "Mysql is not installed, please look into the issue"
+    else
+        echo "Mysql is successfully installed.."
+else
+    echo "Mysql is already installed"
+fi
+    
